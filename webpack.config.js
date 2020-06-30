e6d0aa4e59f8d9e528976b9ea095b26a8d6782b6
XinPing Wang <xinping.wang@hotmail.com>
2020-06-30 22:08:02 +0800

    ts config

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,21 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
   mode: 'development',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
   },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  devtool: 'inline-source-map',
   plugins: [
     new CleanWebpackPlugin(),
     new CopyWebpackPlugin({
